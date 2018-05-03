7ec0f85b4adeda78f7fcc8443e43996202867960
paolaAndrea5 <35497800+paolaAndrea5@users.noreply.github.com>
2018-05-03 00:51:05 -0500

    Add files via upload

--- src/java/JavaBean/BeanPublicacionesDudosas.java
+++ src/java/JavaBean/BeanPublicacionesDudosas.java
@@ -5,8 +5,14 @@
  */
 package JavaBean;
 
+import Dao.publicacionesDAO;
 import Dao.publicacionesDudosasDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -20,13 +26,48 @@ public class BeanPublicacionesDudosas {
     
     public JSONObject obtenerPublicacionDudosa(String IdPersona){
         publicacionesDudosasDAO pubDudosas= new publicacionesDudosasDAO();
-        JSONObject jsonDudosas= pubDudosas.publicacionDudosas("1");
+        JSONObject jsonDudosas= pubDudosas.publicacionDudosas(IdPersona);
         System.out.println(jsonDudosas);
         return jsonDudosas;
     }
-    public JSONObject insertarPublicacion(JSONObject jsonPublicacion){
+    public JSONObject insertarPublicacion(String jsonPublicacion){
         JSONObject obj= new JSONObject();
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject json = (JSONObject) parser.parse(jsonPublicacion);
+            publicacionesDudosasDAO pubDudosas= new publicacionesDudosasDAO();
+            pubDudosas.actualizarPublicacion(json);
+            obj.put("code",0);
+        } catch (ParseException ex) {
+            Logger.getLogger(BeanPublicacionesDudosas.class.getName()).log(Level.SEVERE, null, ex);
+           obj.put("code",9999);
+        }
+        
         return obj;
+        
+        
+    }
+    
+     public JSONObject cambiarPublicacion(String jsonPublicacion){
+        JSONObject obj= new JSONObject();
+        JSONParser parser = new JSONParser();
+        publicacionesDAO pub = new publicacionesDAO();
+        try {
+            JSONObject json = (JSONObject) parser.parse(jsonPublicacion);
+            obj= pub.insertPublicacion((String) json.get("userId"), json);
+            publicacionesDudosasDAO pubDudosas= new publicacionesDudosasDAO();
+            pubDudosas.cambiarEstado(json);
+           
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(BeanPublicacionesDudosas.class.getName()).log(Level.SEVERE, null, ex);
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(BeanPublicacionesDudosas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return obj;
+        
         
     }
     
