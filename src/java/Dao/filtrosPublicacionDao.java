674fa0fe75e7ba95e7ca0761ef8634d2cc3fd834
paolaAndrea5 <35497800+paolaAndrea5@users.noreply.github.com>
2018-05-03 02:44:41 -0500

    Add files via upload

--- src/java/Dao/filtrosPublicacionDao.java
+++ src/java/Dao/filtrosPublicacionDao.java
@@ -18,6 +18,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
@@ -31,106 +32,90 @@ public class filtrosPublicacionDao {
     
  }   
  
+  public JSONObject getByFiltros (String fechaQueryStatement, String nombreQueryStatement, String rolQueryStatement, String tipoQueryStatement){
+       Cone conex = new Cone();
+        Connection conexion= conex.getConnection();
+        Statement stmt = null;
+        ResultSet rs = null;
+        JSONObject respuesta = new JSONObject();
+         try {
+            respuesta.put("code", 0);
+            respuesta.put("description", "Operacion exitosa");
             
-  public List<Publicacion> buscarPublicacion(String fechaInicio, String fechaFin, String nombre, String titulo,String departamento, String tipo){
-           List<Publicacion>publicaciones=new ArrayList<Publicacion>();
-           Cone conex = new Cone();
-            Connection conexion= conex.getConnection();
-            Statement stmt = null;
-            ResultSet rs = null;
-     try {
-         stmt = conexion.createStatement();
-         String query="";
-         if(!fechaInicio.equals("")&& !fechaFin.equals("")&& !nombre.equals("") && !titulo.equals("") &&!departamento.equals("")&&!tipo.equals("")){
-             System.out.println("entre 1");
-             query="Select *"+ 
-                  "FROM (Publicacion INNER JOIN Persona_Publicacion on Publicacion.ID=Persona_Publicacion.IdPublicacion) INNER JOIN Persona ON Persona_Publicacion.IdPersona=Persona.ID "+
-                  "WHERE Publicacion.Titulo='"+ titulo + "' AND Persona.nombre='"+ nombre +"' AND Publicacion.Tipo='"+ tipo +"' AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-                  System.out.println(query);
-         }else if (fechaInicio.equals("") && fechaFin.equals("") && !nombre.equals("") && !titulo.equals("") && !departamento.equals("") && !tipo.equals("")){
-              System.out.println("entre 2");    
-             query="Select *"+ 
-                  "FROM (Publicacion INNER JOIN Persona_Publicacion on Publicacion.ID=Persona_Publicacion.IdPublicacion) INNER JOIN Persona ON Persona_Publicacion.IdPersona=Persona.ID "+
-                  "WHERE Publicacion.Titulo='"+ titulo + "' AND Persona.nombre='"+ nombre +"' AND Publicacion.Tipo='"+ tipo +"'";
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && !nombre.equals("") && titulo.equals("") && !departamento.equals("")&&!tipo.equals("")){
-              System.out.println("entre 3");    
-             query="Select *"+ 
-                  "FROM (Publicacion INNER JOIN Persona_Publicacion on Publicacion.ID=Persona_Publicacion.IdPublicacion) INNER JOIN Persona ON Persona_Publicacion.IdPersona=Persona.ID "+
-                  "WHERE Persona.nombre='"+ nombre +"' AND Publicacion.Tipo='"+ tipo +"' AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-          
-             
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && nombre.equals("") && titulo.equals("") && !departamento.equals("")&& !tipo.equals("")){
-                System.out.println("entre 4");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE  Publicacion.Tipo='"+ tipo +"' AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-          
-         }else if (fechaInicio.equals("")&& fechaFin.equals("") && !nombre.equals("") && titulo.equals("") && !departamento.equals("") && tipo.equals("")){
-                 System.out.println("entre 5");
-             query="Select *"+ 
-                  "FROM (Publicacion INNER JOIN Persona_Publicacion on Publicacion.ID=Persona_Publicacion.IdPublicacion) INNER JOIN Persona ON Persona_Publicacion.IdPersona=Persona.ID "+
-                  "WHERE Persona.nombre='"+ nombre +"'";
-          
-         }else if (fechaInicio.equals("")&& fechaFin.equals("") && nombre.equals("") && !titulo.equals("") && !departamento.equals("")&& tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE Publicacion.Titulo='"+ titulo + "'";
-          
-         }else if (fechaInicio.equals("")&& fechaFin.equals("") && nombre.equals("") && titulo.equals("") && !departamento.equals("")&& !tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE Publicacion.Tipo='"+ tipo +"'";
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && nombre.equals("") && titulo.equals("") && !departamento.equals("")&& tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && !nombre.equals("") && titulo.equals("") && !departamento.equals("")&& tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM (Publicacion INNER JOIN Persona_Publicacion on Publicacion.ID=Persona_Publicacion.IdPublicacion) INNER JOIN Persona ON Persona_Publicacion.IdPersona=Persona.ID "+
-                  "WHERE Persona.nombre='"+ nombre +"' AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-             
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && nombre.equals("") && !titulo.equals("") && !departamento.equals("")&& tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE Publicacion.Titulo='"+ titulo + "'AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-             
-         }else if (!fechaInicio.equals("")&& !fechaFin.equals("") && nombre.equals("") && titulo.equals("") && !departamento.equals("")&& !tipo.equals("")){
-                 System.out.println("entre 6");
-             query="Select *"+ 
-                  "FROM Publicacion"+
-                  "WHERE Publicacion.Tipo='"+ tipo +"' AND Publicacion.FechaInicio BETWEEN '"+fechaInicio+"' AND '"+fechaFin+"'";
-         }else{
-              query="Select *"+ 
-                  "FROM Publicacion";
-             
-         }   
-             rs = stmt.executeQuery(query);                     
-                                  
-                while (rs.next()) {
-                    Publicacion pub= new Publicacion();
-                    pub.setTitulo(rs.getString(2));
-                    pub.setTipo(rs.getString(3));
-                    pub.setFecha(rs.getString(8));
-                    pub.setISBN(rs.getString(4));
-                    pub.setISSN(rs.getString(5));
-                    pub.setLugar(rs.getString(6));
-                    pub.setEditorial(rs.getString(7));
-                    publicaciones.add(pub);
-                             
-                 }
+            String selectStatemt = "select DISTINCT(PU.ID) as id, PU.Titulo, PU.Tipo, PU.codigoPublicacion, PU.Lugar, PU.Editorial, PU.FechaInicio ";
+            String fromStatement = " from Publicacion PU inner join Persona_Publicacion PP on PP.IdPublicacion = PU.ID";
+            String whereStatement = " where PP.EstadoSistema = 1";
+            String groupByStatement = "";
+            Boolean existeGroupByAnterior = false;
+            
+            
+            
+            if (fechaQueryStatement != null) {
+                whereStatement += " and "+fechaQueryStatement;
+            }
+            
+            if (nombreQueryStatement != null) {
+                
+                //selectStatemt = selectStatemt.replace("P", ", P");
+                
+                fromStatement += " inner join Persona PE on PE.ID = PP.IdPersona";
+                
+                whereStatement += " and "+nombreQueryStatement;
+                
+                
+                
+            }
+            
+            if (rolQueryStatement != null) {
+                
+                //selectStatemt = selectStatemt.replace("PP", ", PP");
+                
+                whereStatement += " and "+rolQueryStatement;
+                
+                
+            }
+            
+            if (tipoQueryStatement != null) {
+                
+                //selectStatemt = selectStatemt.replace("PU", ", PU");
+                
+                whereStatement += " and "+tipoQueryStatement;
+                
+               
+            }
+                                
+            stmt = conexion.createStatement();
+            String query = selectStatemt+fromStatement+whereStatement;
+            System.out.println("Query: "+query);
+            
+            rs = stmt.executeQuery(query);                     
+            JSONArray arr = new JSONArray();                       
+            while (rs.next()) {
+                JSONObject obj= new JSONObject();
+                obj.put("titulo",rs.getString(2));
+                obj.put("tipo",rs.getString(3));
+                obj.put("codigoPublicacion",rs.getString(4));
+                obj.put("lugar",rs.getString(5));
+                obj.put("editorial",rs.getString(6));
+                obj.put("fecha",rs.getString(7));
+                System.out.println(rs.getString(1));
+                arr.add(obj);
+            }
             
             rs.close();
             stmt.close();
-            conex.desconectar();
+            respuesta.put("publicaciones", arr);
+           
          
-      }  catch (SQLException ex) {
-         Logger.getLogger(filtrosPublicacionDao.class.getName()).log(Level.SEVERE, null, ex);
-     }
-   return publicaciones;
+        }  catch (SQLException ex) {
+            respuesta.put("code", 9997);
+            respuesta.put("description", "Error de sistema");
+            System.out.println("getParametrosSistema(): "+respuesta.toString());
+            Logger.getLogger(filtrosPublicacionDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+             conex.desconectar();
+         }
+        
+        return respuesta;
   }
 }  
